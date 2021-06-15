5169b75b551da8d5c76f7bb3d21eeb78415be3a5
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-14 20:38:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=250&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+      </div>
     </div>
   );
 }
