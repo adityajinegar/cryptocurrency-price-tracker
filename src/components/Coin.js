d0e7d585089fac92eb909641e3976c575cc4882b
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-14 21:54:34 -0400

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,4 +1,12 @@
-const Coin = ({ image, name, symbol, price, volume }) => {
+const Coin = ({
+  image,
+  name,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
@@ -10,6 +18,14 @@ const Coin = ({ image, name, symbol, price, volume }) => {
         <div className="coin-data">
           <p className="coin-price">${price}</p>
           <p className="coin-volume">${volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChange / 0.1}%</p>
+          )}
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
